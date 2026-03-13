a70b03d229e58831e51b73ecfb009d2644cfa735
Teo <layahan@gmail.com>
2026-03-14 02:22:58 +0900

    feat(11-02): Admin/인증 페이지 noindex 메타태그 추가 (TDD GREEN)
    - admin.html.erb head에 <meta name="robots" content="noindex"> 직접 삽입
    - SessionsController new 액션에 before_action으로 set_meta_tags noindex: true 적용
    - RegistrationsController new 액션에 before_action으로 set_meta_tags noindex: true 적용
    - 통합 테스트 3개 전부 통과

--- teovibe/app/controllers/registrations_controller.rb
+++ teovibe/app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+  before_action :set_noindex, only: %i[new]
 
   def new
     @user = User.new
@@ -20,5 +21,10 @@ class RegistrationsController < ApplicationController
 
   def registration_params
     params.require(:user).permit(:email_address, :nickname, :password, :password_confirmation)
+  end
+
+  # 검색엔진이 회원가입 페이지를 색인하지 않도록 noindex 메타태그 설정
+  def set_noindex
+    set_meta_tags noindex: true
   end
 end
--- teovibe/app/controllers/sessions_controller.rb
+++ teovibe/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "잠시 후 다시 시도해주세요." }
+  before_action :set_noindex, only: %i[new]
 
   def new
   end
@@ -17,5 +18,12 @@ class SessionsController < ApplicationController
   def destroy
     terminate_session
     redirect_to root_path, notice: "로그아웃되었습니다.", status: :see_other
+  end
+
+  private
+
+  # 검색엔진이 로그인 페이지를 색인하지 않도록 noindex 메타태그 설정
+  def set_noindex
+    set_meta_tags noindex: true
   end
 end
